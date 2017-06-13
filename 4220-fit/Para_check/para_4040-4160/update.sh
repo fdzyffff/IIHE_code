a8f05637f737b3ecd122605dec4a5f3d47503a8d
Xuyang Gao <1069379433@qq.com>
2017-06-13 18:50:25 +0200

    add some files

--- 4220-fit/Para_check/para_4040-4160/update.sh
+++
@@ -1,7 +0,0 @@
-cp -rf default/tes.C ddpi-2/
-cp -rf default/tes.C ddpi-3/
-cp -rf default/tes.C ddpi-4/
-cp -rf default/tes.C pipijpsi-2/
-cp -rf default/tes.C pipijpsi-3/
-cp -rf default/tes.C pipijpsi-4/
-cp -rf default/tes.C pipihc-2/
--- 4220-fit/norminal_result_bes_2016_11_21/mkdir.sh
+++
@@ -1,7 +0,0 @@
-cp -rf default ddpi-2
-cp -rf default ddpi-3
-cp -rf default ddpi-4
-cp -rf default pipijpsi-2
-cp -rf default pipijpsi-3
-cp -rf default pipijpsi-4
-cp -rf default pipihc-2
